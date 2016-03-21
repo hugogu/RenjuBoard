bb8db0bb30888e0996e1e15818fb9c313e9cbc12
Hugo Gu <hugogu@outlook.com>
2016-03-21 23:21:42 +0800

    Introduce LinedGrid to customize GameBoardPanel easier.

--- RenjuBoard/Controls/GameBoardPanel.cs
+++ RenjuBoard/Controls/GameBoardPanel.cs
@@ -1,7 +1,5 @@
-﻿using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace RenjuBoard.Controls
 {
@@ -22,37 +20,5 @@ namespace RenjuBoard.Controls
         public static readonly DependencyProperty SizeProperty =
             DependencyProperty.Register("Size", typeof(int), typeof(GameBoardPanel),
                 new FrameworkPropertyMetadata(15, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsArrange));
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            DrawGrid(drawingContext);
-            base.OnRender(drawingContext);
-        }
-
-        private void DrawGrid(DrawingContext drawingContext)
-        {
-            var cellWidth = ActualWidth / Size;
-            var cellHeight = ActualHeight / Size;
-            var lineWidth = ActualWidth - cellWidth;
-            var lineHeight = ActualHeight - cellHeight;
-
-            foreach (var row in Enumerable.Range(0, Size))
-            {
-                var rowHeight = cellHeight / 2 + row * cellHeight;
-                var x1 = new Point(cellWidth / 2, rowHeight);
-                var x2 = new Point(cellWidth / 2 + lineWidth, rowHeight);
-
-                drawingContext.DrawLine(new Pen(Foreground, 1), x1, x2);
-            }
-
-            foreach(var column in Enumerable.Range(0, Size))
-            {
-                var columnOffset = cellWidth / 2 + column * cellWidth;
-                var y1 = new Point(columnOffset, cellHeight / 2);
-                var y2 = new Point(columnOffset, cellHeight / 2 + lineHeight);
-
-                drawingContext.DrawLine(new Pen(Foreground, 1), y1, y2);
-            }
-        }
     }
 }
---
+++ RenjuBoard/Controls/LinedGrid.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace RenjuBoard.Controls
+{
+    public class LinedGrid : UniformGrid
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+            DrawGridLines(dc);
+        }
+
+        public Brush Stroke
+        {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register("Stroke", typeof(Brush), typeof(LinedGrid), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(LinedGrid), new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        private void DrawGridLines(DrawingContext drawingContext)
+        {
+            var cellWidth = ActualWidth / Columns;
+            var cellHeight = ActualHeight / Rows;
+            var lineWidth = ActualWidth - cellWidth;
+            var lineHeight = ActualHeight - cellHeight;
+
+            foreach (var row in Enumerable.Range(0, Rows))
+            {
+                var rowHeight = cellHeight / 2 + row * cellHeight;
+                var x1 = new Point(cellWidth / 2, rowHeight);
+                var x2 = new Point(cellWidth / 2 + lineWidth, rowHeight);
+
+                drawingContext.DrawLine(new Pen(Stroke, StrokeThickness), x1, x2);
+            }
+
+            foreach (var column in Enumerable.Range(0, Columns))
+            {
+                var columnOffset = cellWidth / 2 + column * cellWidth;
+                var y1 = new Point(columnOffset, cellHeight / 2);
+                var y2 = new Point(columnOffset, cellHeight / 2 + lineHeight);
+
+                drawingContext.DrawLine(new Pen(Stroke, StrokeThickness), y1, y2);
+            }
+        }
+    }
+}
