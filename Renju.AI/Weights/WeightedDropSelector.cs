7fdc4aca383cfdb0752b39948df1a2c1fe7ad326
Hugo Gu <hugogu@outlook.com>
2016-05-07 23:14:06 +0800

    Refactoring code of weighted drop selector.

--- Renju.AI/Weights/WeightedDropSelector.cs
+++ Renju.AI/Weights/WeightedDropSelector.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using Renju.Infrastructure;
 using Renju.Infrastructure.Model;
@@ -21,21 +20,13 @@ namespace Renju.AI.Weights
         public IEnumerable<IReadOnlyBoardPoint> SelectDrops(IReadBoardState<IReadOnlyBoardPoint> board, Side side)
         {
             var prioritizedDrops = FindDropsFromPrioritizedTargets(board,
-                                   () => board.GetFours(side).ToList(),
-                                   () => board.GetFours(Sides.Opposite(side)).ToList(),
-                                   () => board.GetOpenThrees(side).ToList(),
-                                   () => board.GetOpenThrees(Sides.Opposite(side)).ToList()).ToList();
-            if (prioritizedDrops.Count > 0)
-            {
-                foreach (var drop in OrderCandidatesPointsByWeight(prioritizedDrops, board, side))
-                    yield return drop;
-                yield break;
-            }
+                                   () => board.GetFours(side).SelectMany(l => l.GetBlockPoints(board)),
+                                   () => board.GetFours(Sides.Opposite(side)).SelectMany(l => l.GetBlockPoints(board)),
+                                   () => board.GetOpenThrees(side).SelectMany(l => l.GetBlockPoints(board)),
+                                   () => board.GetOpenThrees(Sides.Opposite(side)).SelectMany(l => l.GetBlockPoints(board)),
+                                   () => board.Points);
 
-            foreach (var weightedPoint in OrderCandidatesPointsByWeight(board.Points, board, side))
-            {
-                yield return weightedPoint;
-            }
+            return OrderCandidatesPointsByWeight(prioritizedDrops, board, side);
         }
 
         private IEnumerable<IReadOnlyBoardPoint> OrderCandidatesPointsByWeight(IEnumerable<IReadOnlyBoardPoint> pointsCandidates, IReadBoardState<IReadOnlyBoardPoint> board, Side side)
@@ -55,23 +46,13 @@ namespace Renju.AI.Weights
             }
         }
 
-        private IEnumerable<IReadOnlyBoardPoint> FindDropsFromPrioritizedTargets(IReadBoardState<IReadOnlyBoardPoint> board, params Func<ICollection<PieceLine>>[] findLinesFuncs)
+        private IEnumerable<IReadOnlyBoardPoint> FindDropsFromPrioritizedTargets(IReadBoardState<IReadOnlyBoardPoint> board, params Func<IEnumerable<IReadOnlyBoardPoint>>[] findPointsFuncs)
         {
-            foreach (var findLines in findLinesFuncs)
-            {
-                var lines = findLines();
-                if (lines.Count > 0)
-                {
-                    foreach (var line in lines)
-                    {
-                        var drops = line.GetBlockPoints(board).ToList();
-                        Debug.Assert(drops.Count > 0);
-                        foreach (var drop in drops)
-                            yield return drop;
-                    }
-                    yield break;
-                }
-            }
+            foreach (var points in findPointsFuncs.Select(f => f().ToList()))
+                if (points.Count > 0)
+                    return points;
+
+            return new IReadOnlyBoardPoint[0];
         }
 
         private int RandomSeed()
